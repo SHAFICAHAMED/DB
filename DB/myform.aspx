07b2c2ab4673ecb183286209df833e4c687664f7
SHAFICAHAMED <shafic1111ahamed@gmail.com>
2024-12-26 10:37:41 +0530

    Add project files.

---
+++ DB/myform.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="myform.aspx.cs" Inherits="DB.myform" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <label>Firstname:</label>
+                    <asp:TextBox ID="firstname" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+
+                    <td>
+                        <label>Lasttname:</label>
+                        <asp:TextBox ID="lastname" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Age:</label>
+                        <asp:TextBox ID="age" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="ins" runat="server" Text="insert" OnClick="ins_Click" />
+                        <asp:Button ID="del" runat="server" Text="delete" OnClick="del_Click" />
+                        <asp:Button ID="upd" runat="server" Text="update" OnClick="upd_Click" />
+                        <asp:Button ID="vie" runat="server" Text="view" OnClick="vie_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Enter id for update:</label>
+               
+                        <asp:TextBox ID="idold" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DB/myform.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+namespace DB
+{
+    public partial class myform : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|mydb.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            disp_data();
+        }
+
+        protected void ins_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType=CommandType.Text;
+            //cmd.CommandText = "insert into det values('" + firstname.Text+"','"+age.Text+"','"+lastname.Text+"')";
+            cmd.CommandText = "insert into stu values('" + firstname.Text + "')";
+            cmd.ExecuteNonQuery();
+            firstname.Text = "";
+            age.Text = "";
+            lastname.Text = "";
+            disp_data();
+        }
+        public void disp_data()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from stu";
+            cmd.ExecuteNonQuery();
+            DataTable dt=new DataTable();
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            sd.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void del_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from det where firstName='"+firstname.Text+"'";
+            cmd.ExecuteNonQuery();
+            firstname.Text = "";
+           
+            disp_data();
+        }
+
+        protected void upd_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update det set firstName='"+firstname.Text+"', age='"+age.Text+"', lastName='"+lastname.Text+"'where id="+Convert.ToInt32(idold.Text)+"";
+            cmd.ExecuteNonQuery();
+            firstname.Text = "";
+            age.Text = "";
+            lastname.Text = "";
+            disp_data();
+        }
+
+        protected void vie_Click(object sender, EventArgs e)
+        {
+            disp_data();
+        }
+    }
+}
